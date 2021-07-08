a3921d9e04d58d97d4abe1eef1b8ee4876eb24c8
JuanMaGiraldo <juan.giraldo@kommit.co>
2021-07-08 14:33:46 -0500

    View & Template

--- lib/hello_web/controllers/page_controller.ex
+++ lib/hello_web/controllers/page_controller.ex
@@ -36,4 +36,17 @@ defmodule HelloWeb.PageController do
   #     render(conn, "show.json", post: post)
   #   end
   # end
+
+
+  # def show(conn, _params) do
+  #   page = %{title: "foo"}
+
+  #   render(conn, "show.json", page: page)
+  # end
+
+  # def index(conn, _params) do
+  #   pages = [%{title: "foo"}, %{title: "bar"}]
+
+  #   render(conn, "index.json", pages: pages)
+  # end
 end
