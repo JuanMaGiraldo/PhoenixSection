165a73c29a5e036776248453815ebda9bbfca38c
JuanMaGiraldo <33075442+JuanMaGiraldo@users.noreply.github.com>
2021-07-15 15:33:33 -0500

    Context - Custom errors (#3)
    * Router
    
    * Plug section
    
    * Controller
    
    * View & Template
    
    * Context section
    
    * Custom errors
    
    Co-authored-by: JuanMaGiraldo <juan.giraldo@kommit.co>

--- lib/hello_web/controllers/page_controller.ex
+++ lib/hello_web/controllers/page_controller.ex
@@ -1,5 +1,8 @@
 defmodule HelloWeb.PageController do
   use HelloWeb, :controller
+  alias Hello.CMS
+
+  action_fallback HelloWeb.MyFallbackController
 
   action_fallback HelloWeb.MyFallbackController
 
@@ -21,8 +24,6 @@ defmodule HelloWeb.PageController do
     #redirect(conn, to: Routes.page_path(conn, :redirect_test))
 
     conn
-    |> put_flash(:info, "Welcome to Phoenix, from flash info!")
-    |> put_flash(:error, "Let's pretend we have an error.")
     |> render("index.html")
   end
 
